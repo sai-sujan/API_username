e8729de0657150bf1e940a3f2f4852706630ea51
Sai Sujan <59060556+sai-sujan@users.noreply.github.com>
2022-08-23 13:24:14 +0530

    5 random questions

--- main.py
+++ main.py
@@ -4,17 +4,12 @@ import os
 
 app = Flask(__name__)
 # for '/' route return name app
-@app.route('/')
-def index():
-    return "API to retrieve username"
+
 #get_item_list using flask
-@app.route('/api1', methods=['GET'])
-def get_item_list():
-    color = request.args.get('color')
-    food = request.args.get('food')
-    movie = request.args.get('movie')
-    pet = request.args.get('pet')
-    sport = request.args.get('sport')
+
+@app.route('/api', methods=['GET','POST'])
+def get_questions_and_answers():
+
     firebaseConfig = {
         "apiKey": "AIzaSyD-2iQRWn7utb78q3MYboKn5xnumKFbbc0",
         "authDomain": "login-check-grootan.firebaseapp.com",
@@ -28,11 +23,37 @@ def get_item_list():
     firebase = pyrebase.initialize_app(firebaseConfig)
 
     db = firebase.database()
-    dict = {"color": color, "food": food, "movie": movie, "pet": pet, "sport": sport}
-    for i in db.child("Autentication").get():
-        if (i.val() == dict):
-            return ({'username': i.key()})
+    ques = db.child("checking").get()
+    for i in ques.each():
+        questions_dict = i.val()
+        questions = list(questions_dict.keys())
+        answers = list(questions_dict.values())
+    # generate 5 non repetative random numbers between 1 to 10
+
+        import random
+
+        random_list = []
+        while len(random_list) < 5:
+            random_number = random.randint(0, 9)
+            if random_number not in random_list:
+                random_list.append(random_number)
+
+
+        numberList = random_list
+
+        q=[]
+        a=[]
+        for i in random_list:
+
+                q.append(questions[i])
+                a.append(answers[i])
+
+
+        result = dict(zip(q, a))
+        return result
+
 
 if __name__ == '__main__':
+    #app.run()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
